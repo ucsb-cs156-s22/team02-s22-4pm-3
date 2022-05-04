a73ae13ddd41cd713e28e24e4ee0dffb8e431072
KelvinZhang123 <kelvin_zhang@umail.ucsb.edu>
2022-05-04 16:30:06 -0700

    New changes

---
+++ src/main/java/edu/ucsb/cs156/example/entities/UCSBDiningCommonsMenuItem.java
@@ -0,0 +1,23 @@
+package edu.ucsb.cs156.example.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity(name = "ucsbdiningcommonsmenuitem")
+public class UCSBDiningCommonsMenuItem {
+    @Id
+    private long id;
+    private String diningCommonsCode;
+    private String name;
+    private String station;
+
+}
